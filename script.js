edbcf86b728318724d685f62a695b8c65c350b80
Webber-Lu <11378033@o365st.cycu.edu.tw>
2025-04-18 13:49:54 +0800

    cat photo added

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function showMore() {
   alert("更多！");
 }
+
+function toggleCat() {
+  const card = document.getElementById("profileCard");
+  const catGallery = document.getElementById("catGallery");
+
+  const isCatVisible = catGallery.style.display === "block";
+
+  card.style.display = isCatVisible ? "block" : "none";
+  catGallery.style.display = isCatVisible ? "none" : "block";
+}
+
+function returnToCard() {
+  document.getElementById("profileCard").style.display = "block";
+  document.getElementById("catGallery").style.display = "none";
+}
