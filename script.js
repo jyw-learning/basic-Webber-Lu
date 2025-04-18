79be3d7d2c0646d724759a502c2c1453c27f0f73
Webber-Lu <11378033@o365st.cycu.edu.tw>
2025-04-18 15:38:05 +0800

    test

--- script.js
+++ script.js
@@ -5,20 +5,22 @@ function showMore() {
 function toggleCat() {
   const card = document.getElementById("profileCard");
   const catGallery = document.getElementById("catGallery");
+  const backButton = document.getElementById("backButton");
 
   const isCatVisible = catGallery.classList.contains("show");
 
   if (isCatVisible) {
     catGallery.classList.remove("show");
+    backButton.style.display = "none";
     setTimeout(() => {
       catGallery.style.display = "none";
-    }, 500); // 延遲隱藏，讓淡出有時間跑
+    }, 500);
     card.style.display = "block";
   } else {
     card.style.display = "none";
     catGallery.style.display = "flex";
+    backButton.style.display = "block";
 
-    // 等畫面顯示後再加 show class，動畫才會觸發
     requestAnimationFrame(() => {
       catGallery.classList.add("show");
     });
